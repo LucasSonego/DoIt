13d604377e40953790ad08faeeb93594fd4dbd69
ulisses Genguini <ulissesg2018@gmail.com>
2019-11-30 21:45:37 -0200

    funcoes de interacao com o BD da tabela task pronta

--- src/app/App.java
+++ src/app/App.java
@@ -7,6 +7,8 @@ package app;
 
 import br.ufpr.doit.conexao.Task;
 import br.ufpr.doit.conexao.Task_RN;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
 
@@ -14,13 +16,30 @@ public class App {
         System.out.println("Just Do It!");
         Task task = new Task();
         Task_RN taskRn = new Task_RN();
-
-        task.setPK_task("5");
-
-
-        System.out.println(taskRn.buscarPkTask(task));
         
+        List<Task> tasks = new ArrayList<>();
+
+//        task.setNome("projeto final");
+//        task.setDescricao("sem criatividade para uma descricao");
+//        task.setConcluida("0");
+//        task.setFK_list("1");
+
+//        task.setPK_task("6");
+
+
+        System.out.println(taskRn.listarTasksBD(tasks));
+        
+        task = tasks.get(0);
+        System.out.println(task.getNome());
         System.out.println(task.getDescricao());
-        System.out.println(task.getData_inicio());
+        System.out.println(task.getConcluida());
+        task = tasks.get(1);
+        System.out.println(task.getNome());
+        System.out.println(task.getDescricao());
+        System.out.println(task.getConcluida());
+        task = tasks.get(2);
+        System.out.println(task.getNome());
+        System.out.println(task.getDescricao());
+        System.out.println(task.getConcluida());
     }
 }
